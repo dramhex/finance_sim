9c4b56056de67b08c5843e41e8b2b06eb47502f9
dramhex <92268356+dramhex@users.noreply.github.com>
2026-02-25 15:37:26 +0100

    refactor optimizer to include fee rate and enhance parameter optimization methods

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ sim_bitcoin.plot_result_distribution()
 
 prices_with_sl_tp = sim_bitcoin.apply_sl_tp(stop_loss=40000, take_profit=70000)
 
-optimizer = LSTPOptimizer(sim_bitcoin, sl_min=0.5, sl_max=0.95, tp_min=1.05, tp_max=2.0)
-optimizer.optimize()
-optimizer.plot_sharpe_ratio()
+optimizer = LSTPOptimizer(sim_bitcoin, sl_min=0.5, sl_max=0.95, tp_min=1.05, tp_max=4, step=0.05, fee_rate=0.005)
+optimizer.calculate_metrics()
+optimizer.get_optimized_params()
+print(optimizer.best_win_rate)
+print(optimizer.best_sl, optimizer.best_tp)
--- optimizer.py
+++ optimizer.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class LSTPOptimizer:
-    def __init__(self, simulator, sl_min=0.1, sl_max=0.95, tp_min=1.05, tp_max=5, step=0.05):
+    def __init__(self, simulator, sl_min=0.1, sl_max=0.95, tp_min=1.05, tp_max=5, step=0.05, fee_rate=0.005):
         self.sl_range = np.arange(sl_min, sl_max+step, step)
         self.tp_range = np.arange(tp_min, tp_max+step, step)
         self.simulator = simulator
@@ -13,7 +13,13 @@ class LSTPOptimizer:
         self.std_ret = None
         self.sharpe_ratio = None
 
-    def optimize(self):
+        self.best_returns = None
+        self.idx_best = None
+        self.best_sl = None
+        self.best_tp = None
+        self.fee_rate = fee_rate
+
+    def calculate_metrics(self):
         sl_flat = self.SL_grid.flatten() * self.simulator.s0
         tp_flat = self.TP_grid.flatten() * self.simulator.s0
 
@@ -34,9 +40,20 @@ class LSTPOptimizer:
         # On obtient une matrice (n_sims, n_strategies)
         final_wealth = self.simulator.prices_series[actual_exit_indices, sim_indices]
 
-        self.returns = (final_wealth - self.simulator.s0) / self.simulator.s0
+        self.returns = ((final_wealth - self.simulator.s0) / self.simulator.s0) - self.fee_rate
         self.mean_ret = self.returns.mean(axis=0)  # Moyenne par stratégie
         self.std_ret = self.returns.std(axis=0)    # Risque par stratégie
+        self.sharpe_ratio = self.mean_ret / self.std_ret  # Ratio de Sharpe
+
+    def get_optimized_params(self):
+        self.calculate_metrics()
+
+        self.idx_best = np.argmax(self.sharpe_ratio)
+        self.best_sl = self.SL_grid.flatten()[self.idx_best]
+        self.best_tp = self.TP_grid.flatten()[self.idx_best]
+        self.best_returns = self.returns[:, self.idx_best]
+
+        return self.best_sl, self.best_tp
 
         # mean_ret_2d = mean_ret.reshape(SL_grid.shape)
         # plt.imshow(mean_ret_2d, extent=[0.5, 0.95, 1.05, 2.0], origin="lower")
@@ -46,9 +63,12 @@ class LSTPOptimizer:
         # plt.title("Optimisation de la stratégie SL/TP sur Bitcoin")
         # plt.show()
     
+    @property
+    def best_win_rate(self):
+        return (self.best_returns > 0).mean()
+
     def plot_sharpe_ratio(self):
-        sharpe_ratio = self.mean_ret / self.std_ret
-        sharpe_ratio_2d = sharpe_ratio.reshape(self.SL_grid.shape)
+        sharpe_ratio_2d = self.sharpe_ratio.reshape(self.SL_grid.shape)
         plt.imshow(sharpe_ratio_2d, extent=[self.sl_range.min(), self.sl_range.max(), self.tp_range.min(), self.tp_range.max()], origin="lower")
         plt.colorbar(label="Ratio de Sharpe")
         plt.xlabel("Stop Loss (fraction du prix)")
